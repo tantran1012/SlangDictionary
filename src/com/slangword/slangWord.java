5d75cfee8d1ccae739abeff21541698be4b8a786
tantran1012 <57386469+tantran1012@users.noreply.github.com>
2022-03-18 15:14:08 +0700

    Update Tìm theo definition, thêm chức năng lịch sử ^^, và menu vẫn chưa có :))))) *** "lịch sử" thêm nhưng chưa debug ._.

--- src/com/slangword/slangWord.java
+++ src/com/slangword/slangWord.java
@@ -1,15 +1,16 @@
 package com.slangword;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.HashMap;
+import java.util.LinkedHashSet;
 import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.regex.Pattern;
 
 public class slangWord {
     private final HashMap<String, String> slangWord = new HashMap<>();
+    private final LinkedHashSet<String> SlangHistory = new LinkedHashSet<>();
+    private final LinkedHashSet<String> DefinitionHistory = new LinkedHashSet<>();
 
     private void pushSlang(String Key, String Mean){
         slangWord.put(Key, Mean);
@@ -47,6 +48,7 @@ public class slangWord {
         String key = readWord.nextLine();
         if (slangWord.containsKey(key)) {
             getSlang(key);
+            addToHistory(key, SlangHistory);
         }
         else{
             System.out.print("Khong tim thay tu nay");
@@ -58,12 +60,81 @@ public class slangWord {
         System.out.print("Nhap tu can tra:");
         Scanner readWord = new Scanner(System.in);
         String definition = readWord.nextLine();
-        Boolean isExist = false;
-        System.out.println("Danh sach cac tu chua \"" + definition + "\"");
+        AtomicReference<Boolean> isExist = new AtomicReference<>(false);
+        System.out.println("Danh sach cac tu chua \"" + definition + "\":");
         slangWord.forEach((key, value) -> {
             if(value.contains(definition)) {
                 System.out.println(key + " : " + value);
+                addToHistory(definition, DefinitionHistory);
+                isExist.set(true);
             }
         });
+        if (!isExist.get())
+            System.out.println("Khong tim thay tu nay");
+    }
+
+    private void addToHistory(String Str, LinkedHashSet<String> history){
+        history.add(Str);
+    }
+
+    private void viewHistory(LinkedHashSet<String> history){
+        System.out.println("Cac tu da tra:");
+        for (String word : history) {
+            System.out.println(word);
+        }
+    }
+
+    public void chooseHistory(){
+        System.out.println("Chon lich su tra");
+        System.out.println("1. Lich su tra tu");
+        System.out.println("2. Lich su tra dinh nghia");
+        Scanner sc = new Scanner(System.in);
+        int choose = sc.nextInt();
+        if (choose == 1)
+            viewHistory(SlangHistory);
+        else if (choose == 2)
+            viewHistory(DefinitionHistory);
+        else
+            System.out.println("vui long nhap dung thao tac");
+    }
+
+    private void writeHistory(LinkedHashSet<String> history, String type){
+        //this function is using for saving history to file
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter(type));
+            for (String str : history){
+                bw.write(str);
+                bw.newLine();
+            }
+            bw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void readHistoryFromFile(){
+        //this function is using for reading history from file
+        try {
+            BufferedReader SlangHis = new BufferedReader(new FileReader("SlangHistory.txt"));
+            BufferedReader DefinitionHis = new BufferedReader(new FileReader("DefinitionHistory.txt"));
+            String SH, DH;
+            while (true) {
+                SH = SlangHis.readLine();
+                if (SH==null)
+                    break;
+                SlangHistory.add(SH);
+            }
+            while (true) {
+                DH = DefinitionHis.readLine();
+                if (DH==null)
+                    break;
+                SlangHistory.add(DH);
+            }
+            SlangHis.close();
+            DefinitionHis.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
